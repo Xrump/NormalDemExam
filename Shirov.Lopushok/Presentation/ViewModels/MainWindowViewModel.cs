0550efed6718d7741de5dc745a31e3d7292d9299
Xrump <max.hirow3464563@mail.ru>
2022-10-26 13:16:45 +0300

    add Files

---
+++ Shirov.Lopushok/Presentation/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shirov.Lopushok.Domain.Entities;
+using Shirov.Lopushok.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shirov.Lopushok.Presentation.ViewModels
+{
+    public class MainWindowViewModel:ViewModelBase
+    {
+        public List<Product> Product { get; set; }
+        public MainWindowViewModel()
+        {
+            using(ApplicationDbContext context=new ApplicationDbContext())
+            {
+                Product = context.Products
+                    .Include(p=>p.ProductType)
+                    .ToList();
+            }
+        }
+    }
+}
---
+++ Shirov.Lopushok/Presentation/ViewModels/ViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shirov.Lopushok.Presentation.ViewModels
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected bool Set<T>(ref T field,T value,string properyName)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+                return false;
+
+            field = value;
+            OnPropertChanged(properyName);
+            return true;
+        }
+        protected virtual void OnPropertChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
